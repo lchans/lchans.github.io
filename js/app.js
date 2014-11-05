dd1762269ede3a3778469616d3e27811968e7321
Lavender <lavender.chan2@gmail.com>
2014-11-05 22:07:46 +1100

    copied filed

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ var details = [
     image: "img/colombusweb.png",
     tools: "HTML, CSS, Javascript, AngularJS, JQuery",
     other: "Google Maps API, Flickr API, Google Geocoding API, least.js",
+    link: "http://columbusapp.herokuapp.com",
     info: "An image gallery application which allows users to search for a paticular\n\
     tag based on the location of a marker on the world map. The user can also generate \n\
     a heat map around the marker, so the more red an area glows, the more images there are in \n\
@@ -32,11 +33,12 @@ var details = [
     image: "img/medrevueweb.png",
     tools: "HTML, CSS, Javascript, AngularJS, JQuery",
     other: "least.js",
+    link: "http://lchans.github.io/medrevue/",
     info: "Med Revue 2015 UNSW Website - I was intiailly approached by one of next year's Directors\n\
-    to redesign the 2014 website, some information, like the teams description page, is pending\n\
+    to redesign the 2014 website, some information is still pending - \n\
     (I'm assuming the writers are busy working on scripts instead!), however hopefully the launch \n\
-    of this website should be at the end of this year. This project runs on Angular JS - It is the \n\
-    first project I've felt confident working with. "
+    of this website should be at the end of this year. This project was built using the AngularJS \n\
+    Framework. "
   }
 ];
 
@@ -50,10 +52,24 @@ var icons = [
 ];
 
 var designs = [ 
-    { image: "img/designs/poster.png" }, 
-    { image: "img/designs/proposal.png" }, 
-    { image: "img/designs/soctial.png" },
-    { image: "img/designs/arama.jpg" } 
+    { image: "img/designs//poster.png", 
+      name: "Ice Skating Poster",
+      description: "CSESOC UNSW"}, 
+    { image: "img/designs/proposal.png", 
+      name: "Sponsorship Proposal",
+      description: "CSESOC UNSW"}, 
+    { image: "img/designs/soctial.png",
+      name: "Soctail 2014",
+      description: "CSESOC UNSW"},
+    { image: "img/designs/arama.jpg",
+      name: "Arama Logo",
+      description: "Microsoft Image Cup Entry"},
+    { image: "img/designs/hoodie.png",
+      name: "Hoodie Design",
+      description: "CSESOC UNSW"},  
+    { image: "img/designs/trivia.png",
+      name: "Arama Logo",
+      description: "CSESOC UNSW"} 
 ]; 
 
 })();
